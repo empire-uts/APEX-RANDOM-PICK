ae39f887406bc9ce6b813c4576a3ec087e87cb1f
empire-uts <kii.einsatz@docomo.ne.jp>
2022-02-18 19:59:41 +0900

    iine!!

--- charapicker.js
+++ charapicker.js
@@ -1,25 +1,10 @@
-let buttonchange = charapicker.value.innerHTML = "retry!"
-let userresult = greeting.charactername[Math.floor( Math.random() * 20)];
-
-
 const charactername = ["レイス","パスファインダー","ジブラルタル","ライフライン","バンガロール",
 "オクタン","ワットソン","コースティック","クリプト","ブラッドハウンド","ミラージュ","レブナント",
 "ローバ","ホライゾン","ヴァルキリー","ランパート","シア","アッシュ","ヒューズ","マッドマギー"]
 
-
-let rand = Math.floor( Math.random() * 20);
-
-
-
-
-const button = document.querySelector('input');
-button.addEventListener('click', updateButton);
-
-function updateButton() {
-  if (button.value === 'start!') {
-    button.value = 'retry';
-    document.getElementById(charapicker) = userresult;
-  } else {
-    document.getElementById(charapicker) = userresult;
-  }
+function userresult(){
+  document.getElementById("chara-pick-button").innerHTML = charactername[Math.floor( Math.random() * 20)];
+  document.getElementById("charapicker").value = "retry!";
 }
+
+
